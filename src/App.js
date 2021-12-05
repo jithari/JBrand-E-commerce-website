026665cdd7f14c6a352b1b3ad542c32c40e9cd66
Jithari <jitharijayarathna@gmail.com>
2021-12-05 13:35:28 +0530

    updated with orderplacement

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 import SigninScreen from './screens/SigninScreen';
 
 function App() {
@@ -65,6 +66,7 @@ function App() {
             <Route path="/shipping" element={<ShippingAddressScreen />}></Route>
             <Route path="/payment" element={<PaymentMethodScreen />}></Route>
             <Route path="/placeorder" element={<PlaceOrderScreen />}></Route>
+            <Route path="/order/:id" element={<OrderScreen />}></Route>
             <Route path="/" element={<HomeScreen />} exact></Route>
           </Routes>
         </main>
